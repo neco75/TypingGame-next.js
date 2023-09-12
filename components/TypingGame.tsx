20f2502b3f7a17b2a952204a59927a67e990cd0e
sagara_rikuto <sagara_rikuto@plise.co.jp>
2023-09-12 16:00:41 +0900

    first

---
+++ components/TypingGame.tsx
@@ -0,0 +1,89 @@
+import randomWords from 'random-words';
+import React, { useState, useEffect } from 'react';
+
+const TypingGame: React.FC = () => {
+  const [word, setWord] = useState<string>('');
+  const [input, setInput] = useState<string>('');
+  const [score, setScore] = useState<number>(0);
+  const [missCount, setMissCount] = useState<number>(0);
+  const [time, setTime] = useState<number>(30);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+
+  useEffect(() => {
+    generateNewWord();
+  }, []);
+
+  // タイマーのカウントダウン
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (time > 0 && !gameOver && missCount < 5) {
+      timer = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    } else if ((time === 0 || missCount >= 5) && !gameOver) {
+      // タイムアップまたはmissCountが5以上の場合の処理
+      setGameOver(true);
+      alert("Game Over");
+    }
+
+    return () => clearTimeout(timer);
+  }, [time, gameOver, missCount]);
+
+  const generateNewWord = () => {
+    const newWord = randomWords();
+    setWord(newWord);
+    setInput('');
+  };
+
+  const reset = () => {
+    setInput('');
+    setScore(0);
+    setMissCount(0);
+    setTime(30);
+    setGameOver(false);
+    document.getElementById("text")!.style.backgroundColor = "white";
+    generateNewWord();
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userInput = e.target.value;
+    setInput(userInput);
+
+    if (!gameOver && missCount < 5) {
+      if (userInput === word) {
+        document.getElementById("text")!.style.backgroundColor = "white";
+        setScore(score + 10);
+        setTime(time + 2);
+        generateNewWord();
+      } else if (userInput.length >= word.length) {
+        document.getElementById("text")!.style.backgroundColor = "pink";
+        setInput('');
+        setScore(Math.max(score - 1, 0)); // スコアが負の数にならないようにする
+        setMissCount(missCount + 1);
+
+        if (missCount + 1 >= 5) {
+          // missCountが5以上になった場合の処理
+          setGameOver(true);
+          alert("Game Over");
+        }
+
+        setTime(Math.max(time - 3, 0)); // タイムが負の数にならないようにする
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h1>Typing Game</h1>
+      <p>Time: {time}</p>
+      <p>Score: {score}</p>
+      <p>MissCount: {missCount}</p>
+      <h2>Word: {word}</h2>
+      <input id="text" type="text" value={input} onChange={handleInputChange} />
+      <input id="reset" type="button" value="Reset" onClick={reset} />
+    </div>
+  );
+};
+
+export default TypingGame;
